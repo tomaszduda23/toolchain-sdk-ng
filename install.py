ecda1b77d4ca475059eeac78a284a5d79e05c34a
Tomasz Duda <tomaszduda23@gmail.com>
2024-02-01 22:25:26 +0100

    init commit

---
+++ install.py
@@ -0,0 +1,24 @@
+import os
+import tarfile
+import urllib.request
+
+VERSION = "0.16.1"
+BASE_URL = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download"
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+TOOLCHAIN_INSTALLED = os.path.join(ROOT_DIR, "toolchain_installed")
+
+if os.path.isfile(TOOLCHAIN_INSTALLED):
+    exit(0)
+
+def unpack_remote(url, destination):
+    local_path = os.path.join(ROOT_DIR, os.path.basename(url))
+    urllib.request.urlretrieve(url, local_path)
+    tarfile.open(local_path, "r:xz").extractall(destination)
+    os.remove(local_path)
+
+unpack_remote(f"{BASE_URL}/v{VERSION}/zephyr-sdk-{VERSION}_linux-x86_64_minimal.tar.xz", ROOT_DIR)
+unpack_remote(f"{BASE_URL}/v{VERSION}/toolchain_linux-x86_64_arm-zephyr-eabi.tar.xz", os.path.join(ROOT_DIR, f"zephyr-sdk-{VERSION}"))
+
+print("Toolchain installed")
+open(TOOLCHAIN_INSTALLED, "x")
+
